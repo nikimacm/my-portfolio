09af8e145f6dcb9995efe2cb93ab4b19c5dc870d
nikimacm <nickymacm@gmail.com>
2023-10-02 16:36:54 +0000

    centered arrow in main button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
+// Mouse Circle
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-// Mouse Circle
 const mouseCircleFn = (x, y) => {
     mouseCircle.style.cssText = `top:${y}px;left:${x}px`;
     mouseDot.style.cssText = `top:${y}px;left:${x}px`;
