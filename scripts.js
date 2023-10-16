a0a6d1acfc2424fcbacf2ebd2e77d4d6e9e659be
nikimacm <nickymacm@gmail.com>
2023-10-16 11:24:14 +0000

    added fadded in/out effect to social icons

--- scripts.js
+++ scripts.js
@@ -240,4 +240,30 @@ formInputs.forEach((input) => {
     });
 });
 // End of Form
+
+// Slideshow
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+    const firstIcon = slideshow.firstElementChild;
+
+    firstIcon.classList.add("faded-out");
+
+    const thirdIcon = slideshow.children[3];
+
+    thirdIcon.classList.add("light");
+
+    thirdIcon.previousElementSibling.classList.remove("light");
+
+    setTimeout(() => {
+        slideshow.removeChild(firstIcon);
+
+        slideshow.appendChild(firstIcon);
+
+        setTimeout(() => {
+            firstIcon.classList.remove("faded-out");
+        }, 500);
+    }, 500);
+}, 3000);
+// End of slideshow
 // End of Section 5
