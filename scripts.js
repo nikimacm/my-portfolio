4ef48e504ce70380f7d6ea96ab58d3edaa62545c
nikimacm <nickymacm@gmail.com>
2023-11-07 11:27:19 +0000

    fixed typo in html progressbar and added arrow rotation and click in js

--- scripts.js
+++ scripts.js
@@ -84,11 +84,81 @@ mainBtns.forEach((btn) => {
 
 // End of Main Button
 
-// Progress bar
+// Progress Bar
+const sections = document.querySelectorAll("section");
 const progressBar = document.querySelector(".progress-bar");
 const halfCircles = document.querySelectorAll(".half-circle");
 const halfCircleTop = document.querySelector(".half-circle-top");
-const progressBarCircle = document.querySelector(".progress-bar-circle")
+const progressBarCircle = document.querySelector(".progress-bar-circle");
+
+let scrolledPortion = 0;
+let scrollBool = false;
+let imageWrapper = false;
+
+const progressBarFn = (bigImgWrapper) => {
+    imageWrapper = bigImgWrapper;
+    let pageHeight = 0;
+    const pageViewportHeight = window.innerHeight;
+
+    if (!imageWrapper) {
+        pageHeight = document.documentElement.scrollHeight;
+        scrolledPortion = window.pageYOffset;
+    } else {
+        pageHeight = imageWrapper.firstElementChild.scrollHeight;
+        scrolledPortion = imageWrapper.scrollTop;
+    }
+
+    const scrolledPortionDegree =
+        (scrolledPortion / (pageHeight - pageViewportHeight)) * 360;
+
+    halfCircles.forEach((el) => {
+        el.style.transform = `rotate(${scrolledPortionDegree}deg)
+`;
+
+        if (scrolledPortionDegree >= 180) {
+            halfCircles[0].style.transform = "rotate(180deg)";
+            halfCircleTop.style.opacity = "0";
+        } else {
+            halfCircleTop.style.opacity = "1";
+        }
+    });
+
+    scrollBool = scrolledPortion + pageViewportHeight === pageHeight;
+
+    // Arrow Rotation
+    if (scrollBool) {
+        progressBarCircle.style.transform = "rotate(180deg)";
+    } else {
+        progressBarCircle.style.transform = "rotate(0)";
+    }
+    // End of Arrow Rotation
+};
+
+// Progress Bar Click
+progressBar.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!imageWrapper) {
+        const sectionPositions = Array.from(sections).map(
+            (section) => scrolledPortion + section.getBoundingClientRect().top
+        );
+
+        const position = sectionPositions.find((sectionPosition) => {
+            return sectionPosition > scrolledPortion;
+        });
+
+        scrollBool ? window.scrollTo(0, 0) : window.scrollTo(0, position);
+    } else {
+        scrollBool
+            ?
+            imageWrapper.scrollTo(0, 0) :
+            imageWrapper.scrollTo(0, imageWrapper.scrollHeight);
+    }
+});
+// End of Progress Bar Click
+
+progressBarFn();
+
 // End of Progress Bar
 
 // Navigation
